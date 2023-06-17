6016d44ffd0e513aa7bf6831748e2fc6b1669781
Atsuto Seko <sekocha@gmail.com>
2023-06-17 17:35:54 +0900

    beta version: mlpgen from multiple datasets

---
+++ tools/vasprun_compress.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import numpy as np
+import sys
+from joblib import Parallel, delayed
+
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+
+def prettify(elem):
+    """
+    Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem)
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="", newl="")
+
+def convert(vasprun):
+
+    try:
+        root = ET.parse(vasprun).getroot()
+    except ET.ParseError:
+        print( 'ET.ParseError:', vasprun)
+        return False
+
+    e = root.find('calculation').find('energy')
+    f = root.find('calculation').find(".//*[@name='forces']")
+    s = root.find('calculation').find(".//*[@name='stress']")
+    st = root.find(".//*[@name='finalpos']")
+    st2 = root.find(".//*[@name='atomtypes']")
+    st3 = root.find(".//*[@name='atoms']")
+
+    m1, c1 = ET.Element('modeling'), ET.Element('calculation')
+    c1.append(e)
+    c1.append(f)
+    c1.append(s)
+    c1.append(st)
+    c1.append(st2)
+    c1.append(st3)
+    m1.append(c1)
+    f = open(vasprun + '.polymlp', 'w')
+    print(prettify(m1), file=f)
+    f.close()
+
+    return True
+
+if __name__ == '__main__':
+    vaspruns = sys.argv[1:]
+    res = Parallel(n_jobs=4)(delayed(convert)(vasp) for vasp in vaspruns)
+
