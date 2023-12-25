238db8f228cf74f65629eedb2ca37aa47ac6c9eb
Atsuto Seko <sekocha@gmail.com>
2023-12-25 16:47:27 +0900

    cmake and pip package development

---
+++ src/pypolymlp/core/interface_phonopy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import numpy as np
+import yaml
+
+def get_frequency(yamlname):
+    data = yaml.load(open(yamlname))
+    freq = [b['frequency'] for d in data['phonon'] for b in d['band']]
+    weight = [d['weight'] for d in data['phonon'] for b in d['band']]
+    return np.array(freq), np.array(weight)
+
+def get_band_frequency(yamlname):
+    data = yaml.load(open(yamlname))
+    dist = [d['distance'] for d in data['phonon']]
+    freq = [[b['frequency'] for b in d['band']] for d in data['phonon']]
+    return dist, freq
+
