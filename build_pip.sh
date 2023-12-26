c0debb48040db10d33cac32a1e39f76d24efff6c
Atsuto Seko <sekocha@gmail.com>
2023-12-26 13:16:05 +0900

    cmake and pip package development

--- build_pip.sh
+++ build_pip.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+version=3.9.13-intel
+rm src/pypolymlp/cxx/lib
+ln -s lib-$version src/pypolymlp/cxx/lib
+python setup.py bdist_wheel
+mv dist/*whl dist/python-$version/
+
 version=3.10.8-intel
 rm src/pypolymlp/cxx/lib
 ln -s lib-$version src/pypolymlp/cxx/lib
@@ -12,7 +18,7 @@ ln -s lib-$version src/pypolymlp/cxx/lib
 python setup.py bdist_wheel
 mv dist/*whl dist/python-$version/
 
-
+################################################
 rm src/pypolymlp/cxx/lib
 ln -s lib-3.11.3-intel src/pypolymlp/cxx/lib
 
