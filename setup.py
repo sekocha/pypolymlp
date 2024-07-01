9edc9dccc123e27f0b1948225bfa2e1fbba5875d
Atsuto Seko <sekocha@gmail.com>
2024-07-01 17:46:10 +0900

    PyPI distribution

--- setup.py
+++ setup.py
@@ -9,17 +9,29 @@ def main():
     # version = _get_version()
 
     packages = setuptools.find_packages("./src")
+    with open("README.md") as f:
+        long_description = f.read()
 
     skbuild.setup(
         name="pypolymlp",
-        version="0.1",
+        version="0.1.3",
         author="Atsuto Seko",
         author_email="seko@cms.mtl.kyoto-u.ac.jp",
         maintainer="Atsuto Seko",
         maintainer_email="seko@cms.mtl.kyoto-u.ac.jp",
         description="This is the pypolymlp module.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="BSD-3-Clause",
         python_requires=">=3.9",
         install_requires=[
+            "setuptools >= 61.0",
+            "scikit-build",
+            "wheel",
+            "cmake",
+            "pybind11",
+            "pybind11-global",
+            "eigen",
             "numpy",
             "scipy",
             "pyyaml",
@@ -38,7 +50,6 @@ def main():
         cmake_install_dir="./src/pypolymlp/cxx/lib",
         packages=packages,
         package_dir={"": "src"},
-        # ext_modules=_get_extensions(build_dir),
     )
 
 
