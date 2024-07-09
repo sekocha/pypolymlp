85516abc8feaad729f158f9bab823fb0d26bc339
Atsuto Seko <sekocha@gmail.com>
2024-07-09 15:36:12 +0900

    build system using scikit-build-core

--- version.py
+++ version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.3.2.dev7+g617012a4.d20240709"
-__version_tuple__ = version_tuple = (0, 3, 2, "dev7", "g617012a4.d20240709")
+__version__ = version = "0.3.2.dev9+g7c78c67a.d20240709"
+__version_tuple__ = version_tuple = (0, 3, 2, "dev9", "g7c78c67a.d20240709")
