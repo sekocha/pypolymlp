7b051be7117d6a84f520527d28325d787b4ac09b
Atsuto Seko <sekocha@gmail.com>
2024-08-27 10:11:48 +0900

    Bug fixed in phono3py_interface

--- version.py
+++ version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.3.4.dev11+gbab50194.d20240822"
-__version_tuple__ = version_tuple = (0, 3, 4, "dev11", "gbab50194.d20240822")
+__version__ = version = "0.3.4.dev12+g431e5802"
+__version_tuple__ = version_tuple = (0, 3, 4, "dev12", "g431e5802")
