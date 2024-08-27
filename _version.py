01ca6a3112174f08574b08d342ec923fcd805949
Atsuto Seko <sekocha@gmail.com>
2024-08-27 14:22:34 +0900

    Bug fixed in phono3py_interface

--- _version.py
+++ _version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.3.7"
+__version__ = version = "0.3.7.post2"
 __version_tuple__ = version_tuple = (0, 3, 7)
