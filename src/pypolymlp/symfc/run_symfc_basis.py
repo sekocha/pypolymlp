afb91d1b7e5807c63f25c6d2caeec7fda53b45af
Atsuto Seko <sekocha@gmail.com>
2024-08-28 13:40:33 +0900

    Reorganize files

--- src/pypolymlp/symfc/run_symfc_basis.py
+++ src/pypolymlp/symfc/run_symfc_basis.py
@@ -1,34 +1,16 @@
-#!/usr/bin/env python
-import argparse
-import signal
-import time
+"""Run symfc."""
 
-import numpy as np
-from symfc.basis_sets.basis_sets_O2 import FCBasisSetO2
-from symfc.basis_sets.basis_sets_O3 import FCBasisSetO3
+from symfc import Symfc
+from symfc.utils.utils import SymfcAtoms
 
 from pypolymlp.core.interface_vasp import Poscar
-from pypolymlp.utils.phonopy_utils import phonopy_supercell
-
-
-def run_fc2(supercell):
-    """Constructing fc2 basis and fc3 basis"""
-    fc2_basis = FCBasisSetO2(supercell, use_mkl=False).run()
-    compress_mat_fc2 = fc2_basis.compression_matrix
-    compress_eigvecs_fc2 = fc2_basis.basis_set
-    return compress_mat_fc2, compress_eigvecs_fc2
-
-
-def run_fc3(supercell):
-
-    # fc3_basis = FCBasisSetO3(supercell, use_mkl=False).run()
-    fc3_basis = FCBasisSetO3(supercell, use_mkl=True).run()
-    compress_mat_fc3 = fc3_basis.compression_matrix
-    compress_eigvecs_fc3 = fc3_basis.basis_set
-    return compress_mat_fc3, compress_eigvecs_fc3
-
+from pypolymlp.utils.structure_utils import supercell_diagonal
 
 if __name__ == "__main__":
+
+    import argparse
+    import signal
+    import time
 
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -42,22 +24,46 @@ if __name__ == "__main__":
         help="Supercell size (diagonal components)",
     )
     parser.add_argument(
-        "--fc2", action="store_true", help="Calculate only fc2 basis set"
+        "--order",
+        type=int,
+        default=3,
+        help="FC order.",
+    )
+    parser.add_argument(
+        "--cutoff_fc3",
+        type=float,
+        default=None,
+        help="Cutoff radius for FC3.",
+    )
+    parser.add_argument(
+        "--cutoff_fc4",
+        type=float,
+        default=None,
+        help="Cutoff radius for FC4.",
     )
     args = parser.parse_args()
 
-    unitcell_dict = Poscar(args.poscar).structure
-    supercell_matrix = np.diag(args.supercell)
-
-    supercell = phonopy_supercell(unitcell_dict, supercell_matrix)
+    """Phonopy use
+    unitcell = Poscar(args.poscar).structure
+    supercell = phonopy_supercell(unitcell, np.diag(args.supercell))
+    """
+    unitcell = Poscar(args.poscar).structure
+    supercell = supercell_diagonal(unitcell, args.supercell)
+    supercell = SymfcAtoms(
+        numbers=supercell.types,
+        cell=supercell.axis.T,
+        scaled_positions=supercell.positions.T,
+    )
+    cutoff = {3: args.cutoff_fc3, 4: args.cutoff_fc4}
 
     t1 = time.time()
-    compress_mat_fc2, compress_eigvecs_fc2 = run_fc2(supercell)
-    print("n_basis (FC2) =", compress_eigvecs_fc2.shape[1])
-
-    if args.fc2 is False:
-        compress_mat_fc3, compress_eigvecs_fc3 = run_fc3(supercell)
-        print("n_basis (FC3) =", compress_eigvecs_fc3.shape[1])
-
+    symfc = Symfc(supercell, use_mkl=True, log_level=1, cutoff=cutoff)
+    symfc.compute_basis_set(args.order)
     t2 = time.time()
-    print(" elapsed time (basis sets for fc2 and fc3) =", t2 - t1)
+    print("Max FC order:", args.order)
+    print("Elapsed time (Basis sets):", "{:.3f}".format(t2 - t1))
+    for order in range(2, args.order + 1):
+        print(
+            "Number of basis vectors (order " + str(order) + "):",
+            symfc.basis_set[order].basis_set.shape[1],
+        )
