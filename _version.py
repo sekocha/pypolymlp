5a0c79708472104a21aa81373152a5cc2aa17445
Atsuto Seko <sekocha@gmail.com>
2024-11-28 10:00:32 +0900

    Developed API for calculations

--- _version.py
+++ _version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.4.6"
-__version_tuple__ = version_tuple = (0, 4, 6)
+__version__ = version = "0.5.0"
+__version_tuple__ = version_tuple = (0, 5, 0)
