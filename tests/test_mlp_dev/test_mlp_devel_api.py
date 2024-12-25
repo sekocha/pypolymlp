d9335b2da704efaa8ba60fd13974a2e75a2ed613
Atsuto Seko <sekocha@gmail.com>
2024-12-25 14:55:39 +0900

    Implementing SSCHA geometry optimization

--- tests/test_mlp_dev/test_mlp_devel_api.py
+++ tests/test_mlp_dev/test_mlp_devel_api.py
@@ -13,6 +13,60 @@ from pypolymlp.mlp_dev.pypolymlp import Pypolymlp
 cwd = Path(__file__).parent
 
 
+def test_mlp_devel_api_sscha():
+    polymlp = Pypolymlp()
+    polymlp.set_params(
+        elements=["Zn", "S"],
+        cutoff=8.0,
+        model_type=3,
+        max_p=2,
+        gtinv_order=3,
+        gtinv_maxl=[4, 4],
+        gaussian_params2=[0.0, 7.0, 7],
+        atomic_energy=[0.0, 0.0],
+        reg_alpha_params=(-3, 3, 10),
+    )
+    yamlfiles = sorted(glob.glob(str(cwd) + "/data-sscha-ZnS/sscha_results_*.yaml"))
+    polymlp.set_datasets_sscha(yamlfiles)
+    polymlp.run(verbose=True)
+
+    error_train1 = polymlp.summary.error_train["train_single"]
+    error_test1 = polymlp.summary.error_test["test_single"]
+
+    assert error_train1["energy"] == pytest.approx(5.765120561897502e-05, rel=1e-4)
+    assert error_test1["energy"] == pytest.approx(7.061448981395381e-05, rel=1e-4)
+
+
+def test_mlp_devel_api_include_force_false():
+    polymlp = Pypolymlp()
+    polymlp.set_params(
+        elements=["Mg", "O"],
+        cutoff=8.0,
+        model_type=3,
+        max_p=2,
+        gtinv_order=3,
+        gtinv_maxl=[4, 4],
+        gaussian_params2=[0.0, 7.0, 8],
+        atomic_energy=[-0.00040000, -1.85321219],
+        include_force=False,
+    )
+
+    train_vaspruns1 = glob.glob(
+        str(cwd) + "/data-MgO/vaspruns/train1/vasprun-*.xml.polymlp"
+    )
+    test_vaspruns1 = glob.glob(
+        str(cwd) + "/data-MgO/vaspruns/test1/vasprun-*.xml.polymlp"
+    )
+    polymlp.set_datasets_vasp(train_vaspruns1, test_vaspruns1)
+
+    polymlp.run(verbose=True, sequential=True)
+    error_train1 = polymlp.summary.error_train["train_single"]
+    error_test1 = polymlp.summary.error_test["test_single"]
+
+    assert error_train1["energy"] == pytest.approx(4.205974853729061e-06, rel=1e-4)
+    assert error_test1["energy"] == pytest.approx(0.0010080932601856329, rel=1e-4)
+
+
 def test_mlp_devel_api_single_dataset():
     polymlp = Pypolymlp()
     polymlp.set_params(
