745de613511d488779a527835a6611def4c8032e
Atsuto Seko <sekocha@gmail.com>
2025-03-01 17:54:13 +0900

    bug_fixed: io_yaml

--- _version.py
+++ _version.py
@@ -16,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.9.1"
-__version_tuple__ = version_tuple = (0, 9, 1)
+__version__ = version = "0.9.2"
+__version_tuple__ = version_tuple = (0, 9, 2)
