54894ff5b194f7529259b90479b1ba69ccd91e97
Atsuto Seko <sekocha@gmail.com>
2025-09-21 16:49:24 +0900

    Implementing disorder model

--- src/pypolymlp/mlp_dev/disorder/disorder.py
+++ src/pypolymlp/mlp_dev/disorder/disorder.py
@@ -58,72 +58,106 @@ def _generate_disorder_params(params: PolymlpParams, occupancy: tuple):
     # TODO: Modify type_pairs and type_indices
     # Use type_group
 
-    return params_rand
-
-
-# def generate_disorder_mlp_pair(
-#     params: PolymlpParams,
-#     coeffs: np.ndarray,
-#     occupancy: tuple,
-# ):
-#     """Generate MLP with pair invariants for disorder model."""
-#     pass
+    occupancy_type = [
+        [(params.elements.index(ele), comp) for ele, comp in occ] for occ in occupancy
+    ]
+    return params_rand, occupancy_type
 
 
 def set_mapping_original_mlp(params: PolymlpParams, coeffs: np.ndarray):
     """Set mapping of original MLP."""
     features_attr, polynomial_attr, atomtype_pair_dict = get_features_attr(params)
-    print(len(atomtype_pair_dict))
-    for k, v in atomtype_pair_dict.items():
-        print(k, v)
 
     seq_id = 0
-    map_model_to_coeffs = dict()
+    map_feature = dict()
     if params.model.feature_type == "gtinv":
         gtinv = params.model.gtinv
-        # features = []
-        for i, attr in enumerate(features_attr):
+        features = []
+        for attr in features_attr:
             # attr: (radial_id, gtinv_id, type_pair_id)
             rad_id = attr[0]
             lc = gtinv.l_comb[attr[1]]
             tp = [tuple(atomtype_pair_dict[i][0]) for i in attr[2]]
             key = tuple([rad_id, tuple(lc), tuple(tp)])
-            map_model_to_coeffs[key] = coeffs[seq_id]
+            features.append(key)
+            map_feature[key] = coeffs[seq_id]
             seq_id += 1
+    else:
+        pass
 
-    # TODO: Include polynomial
+    map_polynomial = dict()
     if len(polynomial_attr) > 0:
-        for i, attr in enumerate(polynomial_attr):
-            print("- id:          ", seq_id)
-            print("  feature_ids: ", attr)
+        for attr in polynomial_attr:
+            key = tuple([features[i] for i in attr])
+            map_polynomial[key] = coeffs[seq_id]
             seq_id += 1
 
-    return map_model_to_coeffs
+    return map_feature, map_polynomial
 
 
-# def generate_disorder_mlp_gtinv(
-#    params: PolymlpParams,
-#    coeffs: np.ndarray,
-#    occupancy: tuple,
-# ):
-#    """Generate MLP with polynomial invariants for disorder model."""
-#
-#     gtinv = params.model.gtinv
-#        for i, attr in enumerate(features_attr):
-#            print("- id:               ", seq_id, file=f)
-#            print("  feature_id:       ", i, file=f)
-#            print("  radial_id:        ", attr[0], file=f)
-#            print("  gtinv_id:         ", attr[1], file=f)
-#            print(
-#                "  l_combination:    ",
-#                gtinv.l_comb[attr[1]],
-#                file=f,
-#            )
-#            print("  atomtype_pair_ids:", attr[2], file=f)
-#            print("", file=f)
-#            seq_id += 1
-#    pass
-#
+def generate_disorder_mlp_pair(
+    params: PolymlpParams,
+    coeffs: np.ndarray,
+    occupancy: tuple,
+):
+    """Generate MLP with pair invariants for disorder model."""
+    map_feature, map_polynomial = set_mapping_original_mlp(params, coeffs)
+    params_rand, occupancy_type = _generate_disorder_params(params, occupancy)
+    features_attr, polynomial_attr, atomtype_pair_dict = get_features_attr(params_rand)
+
+    # TODO: function for pair
+
+
+def find_central_types(type_pairs: list):
+    """Find atom types of central atoms for a combination of type pairs."""
+    common_type = set(type_pairs[0])
+    for tp in type_pairs[1:]:
+        common_type = common_type & set(tp)
+
+    central = dict()
+    for t1 in common_type:
+        neighbors = []
+        for tp in type_pairs:
+            neigh = tp[1] if tp[0] == t1 else tp[0]
+            neighbors.append(neigh)
+        central[t1] = neighbors
+    return central
+
+
+def generate_disorder_mlp_gtinv(
+    params: PolymlpParams,
+    coeffs: np.ndarray,
+    occupancy: tuple,
+):
+    """Generate MLP with polynomial invariants for disorder model."""
+    map_feature, map_polynomial = set_mapping_original_mlp(params, coeffs)
+    params_rand, occupancy_type = _generate_disorder_params(params, occupancy)
+    features_attr, polynomial_attr, atomtype_pair_dict = get_features_attr(params_rand)
+
+    seq_id = 0
+    gtinv = params_rand.model.gtinv
+    coeffs_rand = []
+    for attr in features_attr:
+        coeff = 0.0
+        rad_id = attr[0]
+        lc = gtinv.l_comb[attr[1]]
+        tp = [tuple(atomtype_pair_dict[i][0]) for i in attr[2]]
+        print(lc, tp)
+        # 1. check: type1, 2, 3
+        central = find_central_types(tp)
+        # weight = 1.0 / len(central)
+        for ctype, neigh_types in central.items():
+            print([occupancy_type[i] for i in neigh_types])
+
+        key = tuple([rad_id, tuple(lc), tuple(tp)])
+
+        map_feature[key] = coeffs[seq_id]
+
+        coeffs_rand.append(coeff)
+        seq_id += 1
+
+    if len(polynomial_attr) > 0:
+        pass
 
 
 def generate_disorder_mlp(
@@ -132,28 +166,10 @@ def generate_disorder_mlp(
     occupancy: tuple,
 ):
     """Generate MLP for disorder model."""
-    params_rand = _generate_disorder_params(params, occupancy)
-
-    #    print(features_attr)
-
-    # feature_mapping = set_mapping_original_mlp(params, coeffs)
-    set_mapping_original_mlp(params, coeffs)
-
-    features_attr_r, polynomial_attr_r, atomtype_pair_dict_r = get_features_attr(
-        params_rand
-    )
-
-
-#    if params.model.feature_type == "pair":
-#        pass
-#    elif params.model.feature_type == "gtinv":
-#        generate_disorder_mlp_gtinv()
-#
-
-#    print("____________")
-#    print(features_attr_r)
-
-# print(n_type)
+    if params.model.feature_type == "pair":
+        generate_disorder_mlp_pair(params, coeffs, occupancy)
+    elif params.model.feature_type == "gtinv":
+        generate_disorder_mlp_gtinv(params, coeffs, occupancy)
 
 
 params, coeffs = load_mlp("polymlp.yaml")
