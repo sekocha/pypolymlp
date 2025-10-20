41182ebe9a2b94e503caafad6069093cc42edb67
Atsuto Seko <sekocha@gmail.com>
2025-10-20 17:25:53 +0900

    Implement iterative calculations in disorder

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -12,8 +12,7 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
+    from typing import Tuple, Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -28,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.15.1.post5'
-__version_tuple__ = version_tuple = (0, 15, 1, 'post5')
+__version__ = version = "0.15.2"
+__version_tuple__ = version_tuple = (0, 15, 2)
 
-__commit_id__ = commit_id = 'g99c7daf6e'
+__commit_id__ = commit_id = "g99c7daf6e"
