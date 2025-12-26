f0aad2cb942c43623db19f0023c8474fe7ffe512
Atsuto Seko <sekocha@gmail.com>
2025-12-26 23:34:19 +0900

    Update tests

--- src/pypolymlp/utils/structure_utils.py
+++ src/pypolymlp/utils/structure_utils.py
@@ -1,15 +1,11 @@
 """Utility functions for modifying structure."""
 
 import copy
-import itertools
-import sys
-from math import pi, sqrt
 from typing import Optional
 
 import numpy as np
 
 from pypolymlp.core.data_format import PolymlpStructure
-from pypolymlp.core.interface_vasp import Poscar
 
 
 def _refine_positions(positions: np.ndarray, tol: float = 1e-13):
@@ -33,6 +29,7 @@ def refine_positions(
 
 
 def reset_types(st: PolymlpStructure):
+    """Reset types using n_atoms."""
     st.types = [i for i, n1 in enumerate(st.n_atoms) for n2 in range(n1)]
     return st
 
@@ -43,21 +40,25 @@ def calc_positions_cartesian(st: PolymlpStructure):
 
 
 def get_reciprocal_axis(
-    st: Optional[PolymlpStructure] = None, axis: Optional[np.ndarray] = None
+    st: Optional[PolymlpStructure] = None,
+    axis: Optional[np.ndarray] = None,
 ):
+    """Calculate reciprocal axis."""
     if axis is None:
         axis = st.axis
-    return 2 * pi * np.linalg.inv(axis).T
+    return 2 * np.pi * np.linalg.inv(axis).T
 
 
-def disp(st: PolymlpStructure, eps: float = 0.001):
+def introduce_disp(st: PolymlpStructure, eps: float = 0.001):
+    """Introduce small random displacements."""
     shape = st.positions.shape
-    disp = (2 * eps) * (np.random.rand(shape) - 0.5)
+    disp = (2 * eps) * (np.random.random(shape) - 0.5)
     st.positions += disp
     return st
 
 
 def isotropic_volume_change(st: PolymlpStructure, eps: float = 1.0):
+    """Return structure with isotropic volume change."""
     eps1 = pow(eps, 0.3333333333333)
     st_vol = copy.deepcopy(st)
     st_vol.axis *= eps1
@@ -71,6 +72,7 @@ def multiple_isotropic_volume_changes(
     eps_max: float = 2.0,
     n_eps: float = 10,
 ):
+    """Return structures with sequential isotropic volume changes."""
     eps_array = np.linspace(eps_min, eps_max, n_eps)
     return [isotropic_volume_change(st, eps=eps) for eps in eps_array]
 
@@ -103,33 +105,18 @@ def supercell_diagonal(
     supercell_matrix = np.diag(size)
     n_expand = np.prod(size)
 
-    axis = st.axis @ supercell_matrix
-    n_atoms = np.array(st.n_atoms) * n_expand
-    types = np.repeat(st.types, n_expand)
-    elements = np.repeat(st.elements, n_expand)
-    volume = st.volume * n_expand
+    sup = copy.deepcopy(st)
+    sup.axis = st.axis @ supercell_matrix
+    sup.n_atoms = np.array(st.n_atoms) * n_expand
+    sup.types = np.repeat(st.types, n_expand)
+    sup.elements = np.repeat(st.elements, n_expand)
+    sup.volume = st.volume * n_expand
+    sup.supercell_matrix = supercell_matrix
 
-    trans_all = np.array(
-        list(itertools.product(range(size[0]), range(size[1]), range(size[2]))),
-        dtype=float,
-    )
-    size = np.array(size, dtype=float)
-    positions_new = []
-    for pos in st.positions.T:
-        pos_new = (pos + trans_all) / size
-        positions_new.extend(pos_new)
-    positions = np.array(positions_new).T
-
-    supercell = PolymlpStructure(
-        axis,
-        positions,
-        n_atoms,
-        elements,
-        types,
-        volume,
-        supercell_matrix=supercell_matrix,
-    )
-    return supercell
+    trans_all = np.indices(size).reshape(3, -1).T
+    positions_new = (st.positions.T[:, None] + trans_all[None, :]).reshape((-1, 3))
+    sup.positions = (positions_new / size).T
+    return sup
 
 
 def remove(st: PolymlpStructure, idx: int):
@@ -140,6 +127,10 @@ def remove(st: PolymlpStructure, idx: int):
     st.n_atoms = np.delete(st.n_atoms, idx)
     st.elements = np.delete(st.elements, range(begin, end))
     st.types = np.delete(st.types, range(begin, end))
+    if st.positions_cartesian is not None:
+        st.positions_cartesian = np.delete(
+            st.positions_cartesian, range(begin, end), axis=1
+        )
     return st
 
 
@@ -152,34 +143,22 @@ def remove_atom(st: PolymlpStructure, idx: int):
     sum1 = np.cumsum(st.n_atoms)
     match = np.where(idx < sum1)[0][0]
     st.n_atoms[match] -= 1
+    if st.positions_cartesian is not None:
+        st.positions_cartesian = np.delete(st.positions_cartesian, idx, axis=1)
     return st
 
 
-def reorder(st: PolymlpStructure, order=None, index1=None, index2=None):
+def swap_elements(
+    st: PolymlpStructure,
+    order: Optional[list] = None,
+    index1: Optional[int] = None,
+    index2: Optional[int] = None,
+):
+    """Swap positions and number of atoms.
 
-    if order is None:
-        order = list(range(len(st.n_atoms)))
-        order[index1], order[index2] = order[index2], order[index1]
-
-    positions, n_atoms, types, elements = [], [], [], []
-    for i in order:
-        n_atoms.append(st.n_atoms[i])
-        begin = int(np.sum(st.n_atoms[:i]))
-        end = int(begin + st.n_atoms[i])
-        positions.extend(st.positions.T[begin:end])
-        elements.extend(st.elements[begin:end])
-        types.extend(st.types[begin:end])
-
-    st.n_atoms = np.array(n_atoms)
-    st.elements = np.array(elements)
-    st.types = np.array(types)
-    st.positions = np.array(positions).T
-    return st
-
-
-def swap_elements(st: PolymlpStructure, order=None, index1=None, index2=None):
-    """Orders of element and type are fixed.
-    Orders of positions, n_atoms are swapped."""
+    Orders of element and type are fixed.
+    Orders of positions and n_atoms are swapped.
+    """
     if order is None:
         order = list(range(len(st.n_atoms)))
         order[index1], order[index2] = order[index2], order[index1]
@@ -207,27 +186,26 @@ def swap_elements(st: PolymlpStructure, order=None, index1=None, index2=None):
 
 
 def get_lattice_constants(st: PolymlpStructure):
-
+    """Return lattice constants from axis."""
     a = np.linalg.norm(st.axis[:, 0])
     b = np.linalg.norm(st.axis[:, 1])
     c = np.linalg.norm(st.axis[:, 2])
-    calpha = np.dot(st.axis[:, 1], st.axis[:, 2]) / (b * c)
-    cbeta = np.dot(st.axis[:, 2], st.axis[:, 0]) / (c * a)
-    cgamma = np.dot(st.axis[:, 0], st.axis[:, 1]) / (a * b)
-
+    calpha = st.axis[:, 1] @ st.axis[:, 2] / (b * c)
+    cbeta = st.axis[:, 2] @ st.axis[:, 0] / (c * a)
+    cgamma = st.axis[:, 0] @ st.axis[:, 1] / (a * b)
     return a, b, c, calpha, cbeta, cgamma
 
 
 def triangulation_axis(st: PolymlpStructure):
-
+    """Return structure with axis in triangle form."""
     a, b, c, calpha, cbeta, cgamma = get_lattice_constants(st)
 
     lx = a
     xy = b * cgamma
     xz = c * cbeta
-    ly = sqrt(b * b - xy * xy)
+    ly = np.sqrt(b * b - xy * xy)
     yz = (b * c * calpha - xy * xz) / ly
-    lz = sqrt(c * c - xz * xz - yz * yz)
+    lz = np.sqrt(c * c - xz * xz - yz * yz)
 
     tri_axis = np.array([[lx, xy, xz], [0, ly, yz], [0, 0, lz]])
     st.axis = tri_axis
@@ -235,6 +213,7 @@ def triangulation_axis(st: PolymlpStructure):
 
 
 def random_deformation(st: PolymlpStructure, max_deform: float = 0.1):
+    """Deform structure randomly."""
     rand = (np.random.rand(3, 3) - 0.5) * 2 * max_deform
     st.axis += rand
     return st
@@ -244,6 +223,7 @@ def multiple_random_deformation(
     structures: list[PolymlpStructure],
     max_deform: float = 0.1,
 ):
+    """Deform structure randomly."""
     n_st = len(structures)
     random_deform = (np.random.rand(n_st, 3, 3) - 0.5) * 2 * max_deform
     for st, rand in zip(structures, random_deform):
@@ -273,24 +253,3 @@ def sort_wrt_types(st: PolymlpStructure, return_ids: bool = False):
     if return_ids:
         return st, np.array(ids_all)
     return st
-
-
-if __name__ == "__main__":
-
-    from pypolymlp.utils.vasp_utils import print_poscar, write_poscar_file
-
-    st = Poscar(sys.argv[1]).structure
-
-    st = supercell_diagonal(st)
-    # st_dict = remove(st_dict, 1)
-    st = reorder(st, order=[1, 0])
-    # st = swap_elements(st, order=[1,0])
-    print_poscar(st)
-
-    st = Poscar(sys.argv[1]).structure
-    structures = multiple_isotropic_volume_changes(
-        st, eps_min=0.7, eps_max=2.0, n_eps=15
-    )
-
-    for i, st in enumerate(structures):
-        write_poscar_file(st, filename="POSCAR-" + str(i + 1).zfill(3))
