6c85f4fb9a214de85bdbd261aaf1f08bf3329d6b
Atsuto Seko <sekocha@gmail.com>
2026-01-16 11:18:54 +0900

    Update KIM utils

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -27,7 +27,5 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.16.0.post3"
-__version_tuple__ = version_tuple = (0, 16, 0, "post3")
-
-__commit_id__ = commit_id = "gf96991701"
+__version__ = version = "0.17.0"
+__version_tuple__ = version_tuple = (0, 17, 0)
