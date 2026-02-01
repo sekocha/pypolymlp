e49496c7df2f11a4e4fa5726eb0481028971eb33
Atsuto Seko <sekocha@gmail.com>
2026-02-01 23:21:34 +0900

    Refactoring calculations and Update Tests

---
+++ tests/test_calc/test_utils_fc.py
@@ -0,0 +1,19 @@
+# """Tests of force constant calculations."""
+#
+# import os
+# from pathlib import Path
+#
+# import numpy as np
+# import pytest
+#
+# from pypolymlp.calculator.utils.fc_utils import load_fc2_hdf5, eval_properties_fc2
+#
+# cwd = Path(__file__).parent
+# path_file = str(cwd) + "/files/"
+#
+
+# def test_fc2():
+#     """Test load_fc2_hdf5."""
+#     # TODO: Replace full FC2.
+#     fc2 = path_file + "others/fc2.hdf5"
+#     load_fc2_hdf5(fc2)
