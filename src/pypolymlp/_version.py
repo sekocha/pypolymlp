db399736fbaac7b7ef27a83720e7cb88a92d3e29
Atsuto Seko <sekocha@gmail.com>
2026-02-18 18:18:52 +0900

    Use Eigen in model

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -27,7 +27,5 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.18.7.post3"
-__version_tuple__ = version_tuple = (0, 18, 7, "post3")
-
-__commit_id__ = commit_id = "g8c75ea234"
+__version__ = version = "0.18.8"
+__version_tuple__ = version_tuple = (0, 18, 8)
