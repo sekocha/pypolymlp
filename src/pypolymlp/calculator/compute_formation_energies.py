622aafce55992be36b53368f078729ce157327e3
Atsuto Seko <sekocha@gmail.com>
2026-03-02 18:34:55 +0900

    Add formation energy

--- src/pypolymlp/calculator/compute_formation_energies.py
+++ src/pypolymlp/calculator/compute_formation_energies.py
@@ -9,6 +9,71 @@ from pypolymlp.calculator.utils.composition_utils import Composition
 from pypolymlp.core.data_format import PolymlpParams, PolymlpStructure
 
 
+def compute_formation_energies(
+    structures: list[PolymlpStructure],
+    end_structures: Optional[list[PolymlpStructure]] = None,
+    energies: Optional[np.ndarray] = None,
+    end_energies: Optional[np.ndarray] = None,
+    elements: Optional[list] = None,
+    convex_hull: bool = True,
+):
+    """Compute formation energies and their convex hull."""
+    _initialize_composition(end_structures, end_energies)
+
+    # self._end_structures = structures
+    # self._end_energies, _, _ = self._prop.eval_multiple(structures)
+
+    # chemical_comps_end_members = [
+    #     [np.count_nonzero(st.elements == ele) for ele in self._elements]
+    #     for st in structures
+    # ]
+    # chemical_comps_end_members = np.array(chemical_comps_end_members)
+    # self._comp = Composition(chemical_comps_end_members)
+    # self._comp.energies_end_members = self._end_energies
+
+
+def _initialize_composition(
+    end_structures: Optional[list[PolymlpStructure]] = None,
+    end_energies: Optional[np.ndarray] = None,
+    elements: Optional[list] = None,
+    properties: Optional[Properties] = None,
+):
+    """Initialize composition class."""
+    if end_structures is None and end_energies is None:
+        raise RuntimeError("Definition of end members not provided.")
+
+    if end_structures is None:
+        n_type = len(end_energies)
+        chemical_comps_end_members = np.eye(n_type)
+        composition = Composition(chemical_comps_end_members)
+        composition.energies_end_members = end_energies
+    else:
+        if elements is None:
+            raise RuntimeError("Elements required when using end_structures.")
+
+        n_type = len(elements)
+        if len(end_structures) != n_type:
+            raise RuntimeError("Incosistent size of structures with n_type.")
+
+        chemical_comps_end_members = [
+            [np.count_nonzero(np.array(st.elements) == ele) for ele in elements]
+            for st in end_structures
+        ]
+        composition = Composition(chemical_comps_end_members)
+
+        if end_energies is None:
+            if properties is None:
+                raise RuntimeError("Properties class not found.")
+            energies, _, _ = properties.eval_multiple(end_structures)
+            composition.energies_end_members = energies
+        else:
+            if len(end_energies) != n_type:
+                raise RuntimeError("Incosistent size of energies with n_type.")
+            composition.energies_end_members = end_energies
+
+    return composition
+
+
 class PolymlpFormationEnergies:
     """Class for computing formation energies."""
 
@@ -70,6 +135,10 @@ class PolymlpFormationEnergies:
         Return
         ------
         form: Formation energies.
+              delta E = E - (x1 * E_end1 + x2 * E_end2 + ...).
+              For example, when Ag4 and Au8 are defined as end members,
+              the formation energies per [Ag4](1-x)[Au8]x are calculated as
+              delta E = E - [(1-x) * E(Ag4) + x * E(Au8)].
         """
         if self._end_energies is None:
             raise RuntimeError("Energies for end members not found.")
@@ -90,7 +159,12 @@ class PolymlpFormationEnergies:
 
     @end_structures.setter
     def end_structures(self, structures: list[PolymlpStructure]):
-        """Set structures of end members."""
+        """Set structures of end members.
+
+        For example, if Ag4 and Au8 are endmembers that are used
+        to define the composition, the numbers of atoms for end member structures
+        must be given as [[4, 0], [0, 8]].
+        """
         if len(structures) != self._n_elements:
             # TODO: Not available for Pseudo-k-ary systems,
             #       but available for these systems by using Composition class
--- tests/test_calc/test_compute_formation_energies.py
+++ tests/test_calc/test_compute_formation_energies.py
@@ -5,13 +5,18 @@ from pathlib import Path
 
 import numpy as np
 
-from pypolymlp.calculator.compute_formation_energies import PolymlpFormationEnergies
+from pypolymlp.calculator.compute_formation_energies import (
+    PolymlpFormationEnergies,
+    _initialize_composition,
+)
+from pypolymlp.calculator.properties import Properties
 from pypolymlp.core.interface_vasp import parse_structures_from_poscars
 
 cwd = Path(__file__).parent
 path_file = str(cwd) + "/files/"
 
 pot = path_file + "mlps/polymlp.lammps.gtinv.Cu-Ag-Au"
+prop = Properties(pot=pot)
 
 poscars = [
     path_file + "poscars/POSCAR1.Cu-Ag-Au",
@@ -20,21 +25,57 @@ poscars = [
     path_file + "poscars/POSCAR4.Cu-Ag-Au",
     path_file + "poscars/POSCAR5.Cu-Ag-Au",
 ]
+structures = parse_structures_from_poscars(poscars)
+
+end1 = copy.deepcopy(structures[0])
+end1.elements = ["Cu" for _ in end1.elements]
+end1.types = [0 for _ in end1.elements]
+end2 = structures[0]
+end3 = copy.deepcopy(structures[0])
+end3.elements = ["Au" for _ in end1.elements]
+end3.types = [2 for _ in end1.elements]
+
+end_structures = [end1, end2, end3]
+
+
+def test_initialize_composition():
+    """Test _initialize_composition."""
+    end_energies = [0.1, 0.3, 0.5]
+    composition = _initialize_composition(end_energies=end_energies)
+    np.testing.assert_allclose(composition._proj, np.eye(3))
+
+    composition = _initialize_composition(
+        end_structures=end_structures,
+        end_energies=end_energies,
+        elements=("Cu", "Ag", "Au"),
+    )
+    np.testing.assert_allclose(composition._composition_axis, np.eye(3) * 4)
+    np.testing.assert_allclose(composition._proj, np.eye(3))
+    np.testing.assert_allclose(composition.energies_end_members, [0.1, 0.3, 0.5])
+
+    composition = _initialize_composition(
+        end_structures=end_structures,
+        elements=("Cu", "Ag", "Au"),
+        properties=prop,
+    )
+    np.testing.assert_allclose(composition._composition_axis, np.eye(3) * 4)
+    np.testing.assert_allclose(composition._proj, np.eye(3))
+    values = [-11.645044684167923, -10.114213158625798, -12.161605091983404]
+    np.testing.assert_allclose(composition.energies_end_members, values)
 
 
 def test_PolymlpFormationEnergies():
     """Test PolymlpFormationEnergies."""
-    structures = parse_structures_from_poscars(poscars)
-    end1 = copy.deepcopy(structures[0])
-    end1.elements = ["Cu" for _ in end1.elements]
-    end1.types = [0 for _ in end1.elements]
-    end2 = structures[0]
-    end3 = copy.deepcopy(structures[0])
-    end3.elements = ["Au" for _ in end1.elements]
-    end3.types = [2 for _ in end1.elements]
+    # structures = parse_structures_from_poscars(poscars)
+    # end1 = copy.deepcopy(structures[0])
+    # end1.elements = ["Cu" for _ in end1.elements]
+    # end1.types = [0 for _ in end1.elements]
+    # end2 = structures[0]
+    # end3 = copy.deepcopy(structures[0])
+    # end3.elements = ["Au" for _ in end1.elements]
+    # end3.types = [2 for _ in end1.elements]
 
-    end_structures = [end1, end2, end3]
-
+    # end_structures = [end1, end2, end3]
     api = PolymlpFormationEnergies(pot=pot)
     api.end_structures = end_structures
     form = api.compute(structures)
