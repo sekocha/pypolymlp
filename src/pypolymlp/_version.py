2885f0d2d79d819e18d17143c1bd65091b2e5af8
Atsuto Seko <sekocha@gmail.com>
2026-06-13 18:18:02 +0900

    Add version1 polyinv data

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -18,7 +18,5 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.20.0.post0"
-__version_tuple__ = version_tuple = (0, 20, 0, "post0")
-
-__commit_id__ = commit_id = "gbca866ab4"
+__version__ = version = "0.20.0"
+__version_tuple__ = version_tuple = (0, 20, 0)
