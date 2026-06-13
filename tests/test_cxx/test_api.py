4199271a317289236881a38cfc7b4953def0e994
Atsuto Seko <sekocha@gmail.com>
2026-06-13 22:24:37 +0900

    Start implementing c++ tests.

--- tests/test_cxx/test_api.py
+++ tests/test_cxx/test_api.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+import pytest
+
 from pypolymlp.cxx.api_cxx import get_ylm
 
 cwd = Path(__file__).parent
@@ -16,5 +18,20 @@ def test_get_ylm():
         azimuthal=1.2,
         lmax=10,
     )
-    print(ylm)
-    assert 1 == 0
+    assert len(ylm) == 66
+    assert len(ylm_dx) == 66
+    assert len(ylm_dy) == 66
+    assert len(ylm_dz) == 66
+
+    assert sum(ylm) == pytest.approx(
+        -3.094632553138235 + 0.09510814961092404j, rel=1e-6
+    )
+    assert sum(ylm_dx) == pytest.approx(
+        -6.916830463136405 - 1.910490992920798j, rel=1e-6
+    )
+    assert sum(ylm_dy) == pytest.approx(
+        12.686387327323297 + 23.645024627283863j, rel=1e-6
+    )
+    assert sum(ylm_dz) == pytest.approx(
+        -5.090360117438893 - 11.661266919165213j, rel=1e-6
+    )
