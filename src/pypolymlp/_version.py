f608a5ef7e0b5d6eca297e915032497a30a67172
Atsuto Seko <sekocha@gmail.com>
2026-06-19 18:02:36 +0900

    Set version 0.20.1

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -18,5 +18,5 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.20.0"
-__version_tuple__ = version_tuple = (0, 20, 0)
+__version__ = version = "0.20.1"
+__version_tuple__ = version_tuple = (0, 20, 1)
