28115861fe49bf2ba139c94d7012e660f61c5c52
Atsuto Seko <sekocha@gmail.com>
2026-06-21 16:39:43 +0900

    Tests for polymlpAPI

--- src/pypolymlp/cxx/wrapper/wrapper.py
+++ src/pypolymlp/cxx/wrapper/wrapper.py
@@ -79,7 +79,7 @@ class PolymlpCPPAPI:
     ):
         """Compute features from anlmtp.
 
-        anlmtp_dfx: shape=(n_noconj, n_atom)
+        anlmtp_dfx: shape=(n_anlmtp, n_atom)
         """
         dfx, dfy, dfz, ds = self._api.compute_features_deriv(
             anlmtp, anlmtp_dfx, anlmtp_dfy, anlmtp_dfz, anlmtp_ds, type1
