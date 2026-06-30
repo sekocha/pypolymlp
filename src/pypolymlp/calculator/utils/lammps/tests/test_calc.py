9443972bd34677cfdfb51fe8cb09b0faf46491eb
sekocha <sekocha@gmail.com>
2026-06-30 15:48:32 +0900

    Adding lammps property interface

--- src/pypolymlp/calculator/utils/lammps/tests/test_calc.py
+++ src/pypolymlp/calculator/utils/lammps/tests/test_calc.py
@@ -29,34 +29,53 @@ def test_eval1():
     st = Poscar(path + "POSCAR-Al").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-6.650991070412197, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-5.687876e-01, -5.687876e-01, -5.687876e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Al2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-6.650991070412197, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-5.687876e-01, -5.687876e-01, -5.687876e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Ti").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-10.413251553867973, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-9.443120e-01, -9.443120e-01, -9.443120e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Ti2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-10.413251553867973, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-9.443120e-01, -9.443120e-01, -9.443120e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-TiAl-2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-9.211925132828384, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-1.584955e00, -1.584955e00, -1.584955e00, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-AlTi").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-7.591693394073458, abs=1e-12)
     f_true = [[-0.35067, 0.0, 0.0], [-2.66416, 0.0, 0.0], [3.01483, 0.0, 0.0]]
     np.testing.assert_allclose(f0.T, f_true, atol=1e-4)
+    s_true = [1.360419e01, 5.584720e00, 5.584720e00, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-TiAl").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-7.591693394073495, abs=1e-12)
     f_true = [[-2.66416, 0.0, 0.0], [3.01483, 0.0, 0.0], [-0.35067, 0.0, 0.0]]
     np.testing.assert_allclose(f0.T, f_true, atol=1e-4)
+    s_true = [1.360419e01, 5.584720e00, 5.584720e00, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
 
 def test_eval_eam():
@@ -77,34 +96,53 @@ def test_eval_eam():
     st = Poscar(path + "POSCAR-Al").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-6.52596651890101, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-4.487510e-01, -4.487510e-01, -4.487510e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Al2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-6.52596651890101, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-4.487510e-01, -4.487510e-01, -4.487510e-01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Ti").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-9.608472280865415, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-1.269489e00, -1.269489e00, -1.269489e00, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-Ti2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-9.608472280865415, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [-1.269489e00, -1.269489e00, -1.269489e00, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-TiAl-2").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-8.78154896947258, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [4.442610e-07, 4.442610e-07, 4.442610e-07, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-AlTi").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(7.624104962299699, abs=1e-12)
     f_true = [[0.0638642, 0.0, 0.0], [-6.331095, 0.0, 0.0], [6.267231, 0.0, 0.0]]
     np.testing.assert_allclose(f0.T, f_true, atol=1e-4)
+    s_true = [9.671257e01, 3.468566e01, 3.468566e01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR-TiAl").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(7.624104962299699, abs=1e-12)
     f_true = [[-6.331095, 0.0, 0.0], [6.267231, 0.0, 0.0], [0.0638642, 0.0, 0.0]]
     np.testing.assert_allclose(f0.T, f_true, atol=1e-4)
+    s_true = [9.671257e01, 3.468566e01, 3.468566e01, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
 
 def test_eval2():
@@ -125,6 +163,9 @@ def test_eval2():
     st = Poscar(path + "POSCAR").structure
     e0, f0, s0 = prop.eval(st)
     assert e0 == pytest.approx(-31.642024569145264, abs=1e-12)
+    np.testing.assert_allclose(f0, 0.0, atol=1e-4)
+    s_true = [8.312056e-02, 8.312056e-02, 8.312056e-02, 0.0, 0.0, 0.0]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
 
     st = Poscar(path + "POSCAR2").structure
     e0, f0, s0 = prop.eval(st)
@@ -137,3 +178,5 @@ def test_eval2():
         [-0.01037873, 0.02105782, 0.39254782],
     ]
     np.testing.assert_allclose(f0.T, f_true, atol=1e-4)
+    s_true = [0.437013, 0.375771, 0.650041, -3.126277, -0.504015, -0.38513]
+    np.testing.assert_allclose(s0, s_true, atol=1e-4)
