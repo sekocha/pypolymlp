b52980f7320bdb25fe672596c68626d940bef60d
Atsuto Seko <sekocha@gmail.com>
2026-07-07 13:50:08 +0900

    Bug fixed: supercell

--- src/pypolymlp/utils/supercell_utils.py
+++ src/pypolymlp/utils/supercell_utils.py
@@ -80,7 +80,7 @@ def _get_supercell_diagonal(
 
     # Loop sequence: z -> y -> x, which is compatible with phonopy
     nx, ny, nz = size
-    trans_all = np.indices((nz, ny, nx)).reshape(3, -1).T
+    trans_all = np.indices((int(nz), int(ny), int(nx))).reshape(3, -1).T
     trans_all = trans_all[:, [2, 1, 0]]
     positions_new = (st.positions.T[:, None] + trans_all[None, :]).reshape((-1, 3))
     sup.positions = (positions_new / size).T
