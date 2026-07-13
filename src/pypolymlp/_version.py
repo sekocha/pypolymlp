a601119f4b9093bb13170ac0b6a228f319863ae8
Atsuto Seko <sekocha@gmail.com>
2026-07-13 15:42:44 +0900

    Minor changes

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -18,7 +18,5 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.20.3"
-__version_tuple__ = version_tuple = (0, 20, 3)
-
-__commit_id__ = commit_id = "g4d607f2c4"
+__version__ = version = "0.20.4"
+__version_tuple__ = version_tuple = (0, 20, 4)
