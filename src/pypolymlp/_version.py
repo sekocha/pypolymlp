7294a09961b7c239086be1397016f3752b09cc7c
Atsuto Seko <sekocha@gmail.com>
2026-09-11 13:52:59 +0900

    Implementing transformation class

--- src/pypolymlp/_version.py
+++ src/pypolymlp/_version.py
@@ -18,7 +18,5 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.21.3.post7"
-__version_tuple__ = version_tuple = (0, 21, 3, "post7")
-
-__commit_id__ = commit_id = "g254908ddb"
+__version__ = version = "0.21.4"
+__version_tuple__ = version_tuple = (0, 21, 4)
